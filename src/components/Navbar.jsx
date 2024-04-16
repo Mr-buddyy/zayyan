5f9a20ba2090bf25b7f34f923c02ae5d3e5aa6bf
Buddyy <singgihbudi.sbh@gmail.com>
2024-04-16 16:38:15 +0700

    add

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,21 +14,16 @@ function Navbar() {
                         </div>
                         <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
                             <li>
-                                <a>Item 1</a>
+                                <a href="/">Beranda</a>
                             </li>
                             <li>
-                                <a>Parent</a>
-                                <ul className="p-2">
-                                    <li>
-                                        <a>Submenu 1</a>
-                                    </li>
-                                    <li>
-                                        <a>Submenu 2</a>
-                                    </li>
-                                </ul>
+                                <a href="/portofolio">Portofolio</a>
                             </li>
                             <li>
-                                <a>Item 3</a>
+                                <a href="/project">Project</a>
+                            </li>
+                            <li>
+                                <a href="/tentang">Tentang Kami</a>
                             </li>
                         </ul>
                     </div>
@@ -37,7 +32,7 @@ function Navbar() {
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal px-1">
                         <li>
-                            <a href="/beranda">Beranda</a>
+                            <a href="/">Beranda</a>
                         </li>
                         <li>
                             <a href="/portofolio">Portofolio</a>
